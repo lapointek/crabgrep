76645387eb996fd8407225b72a702dfe43dc2755
lapointek <lapointek.dev@gmail.com>
2025-12-15 12:37:54 -0500

    refactor: use BufReader for more efficient processing of large files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::Parser;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 // Search for a pattern and display the lines
 #[derive(Parser)]
@@ -9,9 +11,20 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     // Parse into the Cli struct
     let args = Cli::parse();
+    // Open file
+    let file = File::open(&args.path)?;
+    // Wrap file in BufReader
+    let reader = BufReader::new(file);
 
-    println!("pattern: {:?}, path: {:?}", args.pattern, args.path)
+    for line in reader.lines() {
+        let line = line?;
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
